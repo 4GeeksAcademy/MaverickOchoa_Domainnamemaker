a3c66c9517c68f22a7603a4f681b361a8317f3e1
MaverickOchoa <126739291+MaverickOchoa@users.noreply.github.com>
2024-06-13 03:21:57 +0000

    domain maker

--- src/app.js
+++ src/app.js
@@ -1,11 +1,29 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+// Define the arrays for pronouns, adjectives, and nouns
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+// Function to generate domain names
+function generateDomainNames(pronoun, adj, noun) {
+  let domains = [];
+  // Nested loops to combine each element from pronoun, adj, and noun
+  for (let p of pronoun) {
+    for (let a of adj) {
+      for (let n of noun) {
+        domains.push(`${p}${a}${n}.com`);
+        domains.push(`${p}${a}${n}.net`);
+        domains.push(`${p}${a}${n}.us`);
+      }
+    }
+  }
+  return domains;
+}
+
+// Generate the domain names
+let domainNames = generateDomainNames(pronoun, adj, noun);
+
+// Log the domain names to the console
+console.log("Possible domain names:");
+domainNames.forEach((domain) => console.log(domain));
